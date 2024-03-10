76564126e9fa27327fb7ad29f670d7ddbbc2d8cf
Jason Savelli <jason.savelli@mention.com>
2024-03-10 23:16:42 +0100

    feat: hover style on task

--- src/board/board-column-task.tsx
+++ src/board/board-column-task.tsx
@@ -14,8 +14,10 @@ export const BoardColumnTask = ({ boardId, task }: BoardColumnTaskProps) => {
 
     return (
         <Link to="/boards/$boardId/task/$taskId" params={{ boardId, taskId: task.id }}>
-            <li className="w-[280px] rounded-lg bg-white px-4 py-6 shadow-lg dark:bg-grey-800">
-                <h3 className="mb-2 text-h-m dark:text-white">{task.name}</h3>
+            <li className="group w-[280px] rounded-lg bg-white px-4 py-6 shadow-lg dark:bg-grey-800">
+                <h3 className="mb-2 text-h-m group-hover:text-purple-500 dark:text-white">
+                    {task.name}
+                </h3>
 
                 <span className="dark:text-grey500 text-m text-grey-500">
                     {doneCount} of {task.subtasks.length} subtasks
